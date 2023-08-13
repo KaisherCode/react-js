fc2f56191d189cc4caae40f9d534a7604b7400c6
Wilson Dev <wilson.hch@hynkor.com>
2023-08-12 21:41:18 -0500

    Componentes de Todo Machine

---
+++ src/TodoItem.jsx
@@ -0,0 +1,10 @@
+function TodoItem() {
+  return (
+    <li>
+      <span>v</span>
+      <p>Llorar con la Llorona</p>
+      <span>x</span>
+    </li>
+  )
+}
+export {TodoItem}
---
+++ src/TodoSearch.jsx
@@ -0,0 +1,6 @@
+function TodoSearch() {
+  return (
+    <input placeholder="Ir la GYM" />
+  )
+}
+export { TodoSearch }
