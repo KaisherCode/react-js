288ecd410c9be5ecf238044d91cdb57469018e06
Wilson Dev <wilson.hch@hynkor.com>
2023-08-13 23:09:30 -0500

    Los estados en React

--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -1,14 +1,15 @@
-
+import React, { useState } from 'react'
 import './TodoSearch.css'
+
 function TodoSearch() {
+  const [searchValue,setSearchValue] = useState('')
+  console.log('Los usuarios buscan TODOs de ' + searchValue)
   return (
-      <input className=" bg-white rounded-3xl border-2 mx-0 my-6 h-16 w-11/12 text-2xl text-center font-sans hover:font-sans font-normal shadow-slate-50 focus:outline-1 focus:outline-indigo-200 " placeholder="Buscar"
-      onChange={(event)=>{
-        console.log('Escribiste en el todoSearch')
-        console.log(event)
-        console.log(event.target)
-        console.log(event.target.value)
-      }}/>
+    <input className=" bg-white rounded-3xl border-2 mx-0 my-6 h-16 w-11/12 text-2xl text-center font-sans hover:font-sans font-normal shadow-slate-50 focus:outline-1 focus:outline-indigo-200 " placeholder="Buscar"
+    value={searchValue}
+    onChange={(event) => {
+        setSearchValue(event.target.value)
+      }} />
   )
 }
 export { TodoSearch }
