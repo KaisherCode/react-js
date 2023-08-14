adf0b844519b959fd8559abb8e302b5a41d6f462
Wilson Dev <wilson.hch@hynkor.com>
2023-08-13 22:30:56 -0500

    Eventos en React: onClick, onChange

--- src/CreateTodoButton.jsx
+++ src/CreateTodoButton.jsx
@@ -2,7 +2,14 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton() {
   return (
-    <button className="CreateTodoButton flex justify-center items-center text-white font-bold border-none text-5xl bottom-6 right-6 w-16 h-16 bg-indigo-500 rounded-full cursor-pointer fixed hover:rotate-180">+</button>
+    <button className="CreateTodoButton flex justify-center items-center text-white font-bold border-none text-5xl bottom-6 right-6 w-16 h-16 bg-indigo-500 rounded-full cursor-pointer fixed hover:rotate-180"
+      onClick={(event) => {
+        console.log('Le diste click')
+        console.log(event)
+        console.log(event.target)
+      }
+      }
+    >+</button>
   )
 }
 
--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -2,7 +2,13 @@
 import './TodoSearch.css'
 function TodoSearch() {
   return (
-      <input className=" bg-white rounded-3xl border-2 mx-0 my-6 h-16 w-11/12 text-2xl text-center font-sans hover:font-sans font-normal shadow-slate-50 focus:outline-1 focus:outline-indigo-200 " placeholder="Buscar" />
+      <input className=" bg-white rounded-3xl border-2 mx-0 my-6 h-16 w-11/12 text-2xl text-center font-sans hover:font-sans font-normal shadow-slate-50 focus:outline-1 focus:outline-indigo-200 " placeholder="Buscar"
+      onChange={(event)=>{
+        console.log('Escribiste en el todoSearch')
+        console.log(event)
+        console.log(event.target)
+        console.log(event.target.value)
+      }}/>
   )
 }
 export { TodoSearch }
