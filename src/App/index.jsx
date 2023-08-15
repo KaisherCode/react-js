10a46e3ee2e46935ecd66f55bcf9124b5939d6b0
Wilson Dev <wilson.hch@hynkor.com>
2023-08-15 05:20:08 -0500

    Organización de archivos ycarpetas | Features-firt directories en react

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,27 +1,12 @@
 import React, { useState } from 'react'
-import { TodoCounter } from './TodoCounter'
-import { TodoSearch } from './TodoSearch'
-import { TodoList } from './TodoList'
-import { TodoItem } from './TodoItem'
-import { CreateTodoButton } from './CreateTodoButton'
+import {useLocalStorage} from './useLocalStorage'
+import { TodoCounter } from '../TodoCounter'
+import { TodoSearch } from '../TodoSearch'
+import { TodoList } from '../TodoList'
+import { TodoItem } from '../TodoItem'
+import { CreateTodoButton } from '../CreateTodoButton'
 import './App.css'
 
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName)
-  let parsedItem
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue))
-    parsedItem = initialValue
-  } else {
-    parsedItem = JSON.parse(localStorageItem)
-  }
-  const [item, setItem] = React.useState(parsedItem)
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem))
-    setItem(newItem)
-  }
-  return [item, saveItem]
-}
 function App() {
   const [todos, saveTodos] = useLocalStorage('TODOS_V1', [])
   const [searchValue, setSearchValue] = React.useState('')
